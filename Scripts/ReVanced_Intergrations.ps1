214852bbb21764db3899218a584a405b2cb90220
Lev Rusanov <30170278+JDM170@users.noreply.github.com>
2023-01-03 12:32:28 +0700

    Update ReVanced_Intergrations.ps1

--- Scripts/ReVanced_Intergrations.ps1
+++ Scripts/ReVanced_Intergrations.ps1
@@ -5,7 +5,7 @@ $Parameters = @{
     Verbose         = $true
 }
 $apiResult = Invoke-RestMethod @Parameters
-$IntegrationsTag = $apiResult.tag_name.replace("v", "")
+$IntegrationsTag = $apiResult.tag_name
 $Parameters = @{
     # Uri             = "https://github.com/revanced/revanced-integrations/releases/download/$Tag/revanced-integrations-$Tag2.apk"
     Uri             = $apiResult.assets.browser_download_url
