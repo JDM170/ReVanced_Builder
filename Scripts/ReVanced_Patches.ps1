faf94e4e29d38e82f9fb1e573f3455bf8daf08d4
Lev Rusanov <30170278+JDM170@users.noreply.github.com>
2023-07-17 04:20:38 +0700

    Update ReVanced_Patches.ps1

--- Scripts/ReVanced_Patches.ps1
+++ Scripts/ReVanced_Patches.ps1
@@ -4,13 +4,10 @@ $Parameters = @{
     UseBasicParsing = $true
     Verbose         = $true
 }
-# $Patchesvtag = (Invoke-RestMethod @Parameters).tag_name
-# $Patchestag = $Patchesvtag.replace("v", "")
 $apiResult = Invoke-RestMethod @Parameters
 $URL = ($apiResult.assets | Where-Object -FilterScript {$_.content_type -eq "application/java-archive"}).browser_download_url
 $TAG = $apiResult.tag_name
 $Parameters = @{
-    # Uri             = "https://github.com/revanced/revanced-patches/releases/download/$Patchesvtag/revanced-patches-$Patchestag.jar"
     Uri             = $URL
     Outfile         = "Temp\revanced-patches.jar"
     UseBasicParsing = $true
